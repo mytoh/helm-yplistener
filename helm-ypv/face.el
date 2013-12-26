37aa1d3be9f9209637ba57f47d8b7cc2b5d7b08e
mytoh <mytoh@gmail.com>
2013-12-27 04:34:31 +0900

    helm-ypv/face.el: remove -face suffix from face names

--- helm-ypv/face.el
+++ helm-ypv/face.el
@@ -7,47 +7,47 @@
       (propertize str 'face face)
     ""))
 
-(defface helm-ypv-name-face
+(defface helm-ypv-name
   '((t :inherit font-lock-type-face))
   "face for channel name"
   :group 'helm-ypv)
 
-(defface helm-ypv-genre-face
+(defface helm-ypv-genre
   '((t :inherit font-lock-keyword-face))
   "face for channel genre"
   :group 'helm-ypv)
 
-(defface helm-ypv-desc-face
+(defface helm-ypv-desc
   '((t :inherit font-lock-string-face))
   "face for channel description"
   :group 'helm-ypv)
 
-(defface helm-ypv-contact-face
+(defface helm-ypv-contact
   '((t :inherit font-lock-variable-name-face))
   "face for channel contact url"
   :group 'helm-ypv)
 
-(defface helm-ypv-type-face
+(defface helm-ypv-type
   '((t :inherit font-lock-type-face))
   "face for channel type"
   :group 'helm-ypv)
 
-(defface helm-ypv-bitrate-face
+(defface helm-ypv-bitrate
   '((t :inherit font-lock-preprocessor-face))
   "face for channel bitrate"
   :group 'helm-ypv)
 
-(defface helm-ypv-time-face
+(defface helm-ypv-time
   '((t :inherit font-lock-preprocessor-face))
   "face for channel time"
   :group 'helm-ypv)
 
-(defface helm-ypv-comment-face
+(defface helm-ypv-comment
   '((t :inherit font-lock-doc-face))
   "face for channel comment"
   :group 'helm-ypv)
 
-(defface helm-ypv-id-face
+(defface helm-ypv-id
   '((t :inherit font-lock-function-name-face))
   "face for channel id"
   :group 'helm-ypv)
