190fab39c1b639e24af32eff6c30123c7d58a7ee
mytoh <mytoh@gmail.com>
2015-01-31 08:35:47 +0900

    * helm-ypv/helm-ypv-url.el: use stream url type

--- helm-ypv/helm-ypv-url.el
+++ helm-ypv/helm-ypv-url.el
@@ -3,10 +3,10 @@
 ;;; Code:
 
 (cl-defmethod helm-ypv-make-url ((channel ypv-channel))
-  (with-slots (id tracker) channel
-    (format "http://%s/pls/%s.pls?tip=%s"
+  (with-slots (id tracker type) channel
+    (format "http://%s/stream/%s.%s?tip=%s"
             helm-ypv-local-address
-            id tracker)))
+            id type tracker)))
 
 (cl-defmethod helm-ypv-make-url ((bkm ypv-bookmark))
   (with-slots (id tracker) bkm
