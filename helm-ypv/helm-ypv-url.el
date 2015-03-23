726e57d4e148744c42fa4457563fe441fd69704c
mytoh <mytoh@gmail.com>
2015-03-23 22:22:48 +0900

    * helm-ypv/helm-ypv-url.el: treat flv as special

--- helm-ypv/helm-ypv-url.el
+++ helm-ypv/helm-ypv-url.el
@@ -5,14 +5,22 @@
 (cl-defmethod helm-ypv-make-url ((channel ypv-channel))
   (with-slots (id tracker type) channel
     (format "%s://%s/stream/%s.%s?tip=%s"
-            helm-ypv-default-protocol
+            (if (string-match (rx (or "flv"
+                                      "FLV"))
+                              type)
+                "http"
+              helm-ypv-default-protocol)
             helm-ypv-local-address
             id type tracker)))
 
 (cl-defmethod helm-ypv-make-url ((bkm ypv-bookmark))
   (with-slots (id tracker type) bkm
     (format "%s://%s/stream/%s.%s?tip=%s"
-            helm-ypv-default-protocol
+            (if (string-match (rx (or "flv"
+                                      "FLV"))
+                              type)
+                "http"
+              helm-ypv-default-protocol)
             helm-ypv-local-address
             id type tracker)))
 
